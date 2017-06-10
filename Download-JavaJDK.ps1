4d2507bcb7ae7ba961b8b6d13c9b628800245411
Leon Nell <leo246@users.noreply.github.com>
2017-06-10 11:15:33 +0100

    Create Download-JavaJDK.ps1

--- Download-JavaJDK.ps1
+++ Download-JavaJDK.ps1
@@ -1,12 +1,12 @@
 Function Download-JavaJDK {
 <#
 .Synopsis
-   Downloading and installing Java JDK 8u121.
+   Downloading and installing Java JDK 8u131.
 .DESCRIPTION
-   Downloading and installing Java JDK 8u121.  Once installed, a persistent
+   Downloading and installing Java JDK 8u131.  Once installed, a persistent
    System Environment variable will be created.
 .EXAMPLE
-   Download-JavaJDK -Destination d:\tmp
+   Download-JavaJDK -Destination d:\tmp -verbose
 #>
 
     [cmdletbinding()]
@@ -16,22 +16,22 @@ Function Download-JavaJDK {
     )
 
     Begin {
-        $Url = "http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-windows-x64.exe"
+        $Url = "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-windows-x64.exe"
     }
     Process {
-        Write-Verbose "Downloading Java JDK 8u121"
+        Write-Verbose "Downloading Java JDK 8u131"
         $client = new-object System.Net.WebClient 
         $cookie = "oraclelicense=accept-securebackup-cookie"
         $client.Headers.Add([System.Net.HttpRequestHeader]::Cookie, $cookie)
-        $DestFile = "$destination\jdk-8u121-windows-x64.exe"
+        $DestFile = "$destination\jdk-8u131-windows-x64.exe"
         $client.downloadFile($url,$DestFile)
 
-        Write-Verbose "Installing Java JDK 8u121 to C:\Program Files\Java"
+        Write-Verbose "Installing Java JDK 8u131 to C:\Program Files\Java"
         start-process -filepath $DestFile -passthru -wait -argumentlist '/s'
-        Write-Output "Java JDK 8u121 installed"
+        Write-Output "Java JDK 8u131 installed"
 
         Write-Verbose "Creating System Environment variable"
-        [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk1.8.0_121","Machine")
+        [Environment]::SetEnvironmentVariable("JAVA_HOME", "C:\Program Files\Java\jdk1.8.0_131","Machine")
     }
     End {
 
